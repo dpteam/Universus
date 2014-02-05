8e0b656ee89ed74649ee6378832406715078ee63
mage-wizard <mage.wizard.88@gmail.com>
2014-02-05 17:22:35 +0400

    Started to rewrite from scratch... The old code was raw and dirty written.

---
+++ u_sdl.cpp
@@ -0,0 +1,9 @@
+//
+//  u_sdl.cpp
+//  Universus
+//
+//  Created by Meyer on 05.02.14.
+//
+//
+
+#include "u_sdl.h"
---
+++ u_sdl.h
@@ -0,0 +1,12 @@
+//
+//  u_sdl.h
+//  Universus
+//
+//  Created by Meyer on 05.02.14.
+//
+//
+
+#ifndef __u_sdl__
+#define __u_sdl__
+
+#endif
